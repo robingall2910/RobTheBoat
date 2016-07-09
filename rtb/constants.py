35299d37143572424ab113b45b869fc180bfce3a
Robinson Gallego <robinson.leal7@gmail.com>
2016-07-09 03:09:58 -0400

    edits

--- rtb/constants.py
+++ rtb/constants.py
@@ -2,10 +2,10 @@ import os.path
 
 VERSION = '3.0.0'
 MAIN_VERSION = '2'
-SUB_VERSION = '-rev 3 '
+SUB_VERSION = '-rev 4 '
 CODENAME = '"👀 👀 👀 👀 👀 👀 👀 👀 👀 👀"' #Screw everything, time to put the codename of what I was listening to.
 VER = VERSION + SUB_VERSION
-BDATE = 'July 6, 2016 @ 3:22 p.m. EDT' #Yay for my birthmonth
+BDATE = 'July 7, 2016 @ 7:51 p.m. EDT' #Yay for my birthmonth
 MAINVER = VERSION + SUB_VERSION + CODENAME
 BUILD_USERNAME = "Wyndrik"
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
