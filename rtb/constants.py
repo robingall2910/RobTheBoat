cc9c6d5b4caa4834e406a88640e4e4d68716f4d5
Robinson Gallego <robinson.leal7@gmail.com>
2016-07-13 21:00:33 -0400

    wew

--- rtb/constants.py
+++ rtb/constants.py
@@ -1,11 +1,11 @@
 import os.path
 
-VERSION = '3.0.2'
+VERSION = '3.0.5'
 MAIN_VERSION = '2'
-SUB_VERSION = '-rev 2 '
-CODENAME = '"Nothing. Nothing at all...."' #Screw everything, time to put the codename of what I was listening to.
+SUB_VERSION = '-rev 1 '
+CODENAME = '"Stupid furry, fuck off with brainpower."' #Screw everything, time to put the codename of what I was listening to.
 VER = VERSION + SUB_VERSION
-BDATE = 'July 10, 2016 @ 2:40 p.m. EDT' #Yay for my birthmonth
+BDATE = 'July 13, 2016 @ 8:58 p.m. EDT' #Yay for my birthmonth
 MAINVER = VERSION + SUB_VERSION + CODENAME
 BUILD_USERNAME = "Wyndrik"
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
