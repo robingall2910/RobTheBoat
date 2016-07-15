43326fe230eb2ad7adbb9c65dac3fe16c6c524f0
Robinson Gallego <robinson.leal7@gmail.com>
2016-07-15 00:32:04 -0400

    O-oooooooooo AAAAE-A-A-I-A-U- JO-oooooooooooo AAE-O-A-A-U-U-A- E-eee-ee-eee AAAAE-A-E-I-E-A- JO-ooo-oo-oo-oo EEEEO-A-AAA-AAAA

--- rtb/constants.py
+++ rtb/constants.py
@@ -3,9 +3,9 @@ import os.path
 VERSION = '3.0.5'
 MAIN_VERSION = '2'
 SUB_VERSION = '-rev 1 '
-CODENAME = '"Stupid furry, fuck off with brainpower."' #Screw everything, time to put the codename of what I was listening to.
+CODENAME = '"O-oooooooooo AAAAE-A-A-I-A-U- JO-oooooooooooo AAE-O-A-A-U-U-A- E-eee-ee-eee AAAAE-A-E-I-E-A- JO-ooo-oo-oo-oo EEEEO-A-AAA-AAAA"'
 VER = VERSION + SUB_VERSION
-BDATE = 'July 13, 2016 @ 8:58 p.m. EDT' #Yay for my birthmonth
+BDATE = 'July 15, 2016 @ 12:29 a.m. EDT' #Yay for my birthmonth
 MAINVER = VERSION + SUB_VERSION + CODENAME
 BUILD_USERNAME = "Wyndrik"
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
