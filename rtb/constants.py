f99b68c2477e11ebfd8bcd277831655631461c9b
Robinson Gallego <robinson.leal7@gmail.com>
2016-07-21 21:12:56 -0400

    don't forget :ok_hand:

--- rtb/constants.py
+++ rtb/constants.py
@@ -1,12 +1,12 @@
 import os.path
 
-VERSION = '3.0.6'
+VERSION = '3.0.7'
 MAIN_VERSION = '3'
 SUB_VERSION = '-rev 1 '
-CODENAME = '"hmu wednesday maybe?"'
+CODENAME = '"Stop touching me!"'
 VER = VERSION + SUB_VERSION
-BDATE = 'July 20, 2016 @ 8:15 p.m. EDT' #Yay for my birthmonth
+BDATE = 'July 21, 2016 @ 9:09 p.m. EDT' #Yay for my birthmonth
 MAINVER = VERSION + SUB_VERSION + CODENAME
-BUILD_USERNAME = "Wyndrik"
+BUILD_USERNAME = "Robin/Wyndrik"
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
 DISCORD_MSG_CHAR_LIMIT = 2000
