b7726a578f95c9f42088718b5c683ec4c0a5d22f
Robinson Gallego <robinson.leal7@gmail.com>
2016-08-02 20:50:32 -0400

    v3.0

---
+++ constants.py
@@ -0,0 +1,12 @@
+import os.path
+
+VERSION = '3.1.0'
+MAIN_VERSION = '3'
+SUB_VERSION = '-rev 2 '
+CODENAME = '"exploit yourself"'
+VER = VERSION + SUB_VERSION
+BDATE = 'August 2, 2016 @ 7:32 p.m. EDT'
+MAINVER = VERSION + SUB_VERSION + CODENAME
+BUILD_USERNAME = "Robin"
+AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
+DISCORD_MSG_CHAR_LIMIT = 2000
