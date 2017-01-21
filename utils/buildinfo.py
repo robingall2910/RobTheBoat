a1190589b16fcbab52c750ae9cf84e0649b123bb
Robinson Gallego <robinson.leal7@gmail.com>
2017-01-20 20:39:44 -0500

    missed a version, incremented it anyway

--- utils/buildinfo.py
+++ utils/buildinfo.py
@@ -1,4 +1,4 @@
-BUILD_VERSION = "5.1"
+BUILD_VERSION = "5.3"
 BUILD_AUTHORS = "Seth#0780 and Robin#0052"
-BUILD_CODENAME = "woah bye af"
-BUILD_DATE = "January 6th, 2017 at 6:01 PM EST"
+BUILD_CODENAME = "what do u want"
+BUILD_DATE = "January 20th, 2016 @ 8:40 PM EST"
