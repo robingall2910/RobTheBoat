9e8c476f4bc5b68d46d6bc3af18261df0e59e329
Robinson Gallego <robinson.leal7@gmail.com>
2017-02-04 23:44:23 -0500

    more ugh

--- utils/buildinfo.py
+++ utils/buildinfo.py
@@ -1,4 +1,4 @@
 BUILD_VERSION = "5.5"
-BUILD_AUTHORS = "based robin#0052 and "
+BUILD_AUTHORS = "based robin#0052 and Seth#0780"
 BUILD_CODENAME = "French Revolution from Louis XIV"
 BUILD_DATE = "Febuary 4th, 2016 @ 12:20 PM EST"
