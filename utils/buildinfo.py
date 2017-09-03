1b8af49345d0c2503f8e970238a8cf01e712b120
Robinson <robinson.leal7@gmail.com>
2017-09-03 13:57:09 -0400

    riiiiiight

--- utils/buildinfo.py
+++ utils/buildinfo.py
@@ -1,4 +1,4 @@
-BUILD_VERSION = "6.9.3"
+BUILD_VERSION = "6.9.4"
 BUILD_AUTHORS = "based robin#0052"
-BUILD_CODENAME = "we haven't served that spirit here since 1969"
-BUILD_DATE = "July 19th, 2017 @ 12:55 AM EDT"
+BUILD_CODENAME = "ask a conservative for their sources, they'll get mad"
+BUILD_DATE = "September 3rd, 2017 @ 1:56 PM EDT"
