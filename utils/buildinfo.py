6ffce46571bdd5b177f152a91a1d700c5134f799
Robinson <robinson.leal7@gmail.com>
2017-11-09 16:47:55 -0500

    xd

--- utils/buildinfo.py
+++ utils/buildinfo.py
@@ -1,4 +1,4 @@
-BUILD_VERSION = "7.1.3"
+BUILD_VERSION = "7.1.4"
 BUILD_AUTHORS = "based robin#0052 and ZeroEpoch1969#0051"
 BUILD_CODENAME = "it's yiffvember y'all"
-BUILD_DATE = "November 5th, 2017 @ 12:01 PM EST"
+BUILD_DATE = "November 9th, 2017 @ 4:47 PM EST"
