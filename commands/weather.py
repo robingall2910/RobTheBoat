ee8d36c9aef2f1e1fdcfdab5e9ef9007cf941fde
Robinson <robinson.leal7@gmail.com>
2017-12-18 17:36:53 -0500

    this is fine

--- commands/weather.py
+++ commands/weather.py
@@ -14,92 +14,119 @@ class Weather():
     def __init__(self, bot):
         self.bot = bot
 
-@commands.command()
-async def weather(self, ctx, *, addr: str):
-    """Go check the weather out :^)"""
-    try:
-        # variable setup
-        g = geocoder.google(addr)
-        results = g.latlng
-        loc = g.address
-        forecast = forecastio.load_forecast(api_key, results[0], results[1])
-        c = forecast.currently()
-        d = forecast.daily()
-        a = forecast.alerts()
-        em = discord.Embed(description="")
-        #localization
-        if ", USA" in loc:
-            farenheit = True
-        elif ", USA" not in loc:
-            farenheit = False
-        em.title = "Current Weather for {}".format(loc)
-        # visibility
+    @commands.command()
+    async def weather(self, ctx, *, addr: str):
+        """Go check the weather out :^)"""
         try:
-            visib = c.visibility
-        except Exception:
-            visib = "N/A"
-        #color the sidebar
-        if ctx.me.color == None:
-            maybe = None
-        else:
-            maybe = ctx.me.color
-        #weather alerts
-        try:
-            a1 = a[0]
-            expiretime = datetime.datetime.fromtimestamp(int(a1.expires)).strftime('%A %B %d, %Y %I:%M %p')
-            areas = ', '.join(a1.regions)
-            alertresult = "{} in {}. Expires {}. Click [here]({} 'National Weather Service/MET Office')".format(a1.title, areas, expiretime, a1.uri)
+            # variable setup
+            g = geocoder.google(addr)
+            results = g.latlng
+            loc = g.address
+            forecast = forecastio.load_forecast(api_key, results[0], results[1])
+            c = forecast.currently()
+            d = forecast.daily()
+            a = forecast.alerts()
+            em = discord.Embed(description="{}\n".format(d.summary))
+            #localization
+            if ", USA" in loc:
+                farenheit = True
+            elif ", USA" not in loc:
+                farenheit = False
+            em.title = "Current Weather for {}".format(loc)
+            em.url = 'http://darksky.net/poweredby/'
+            # visibility
+            try:
+                visib = c.visibility
+            except Exception:
+                visib = "N/A"
+            #color the sidebar
+            if ctx.me.color == None:
+                maybe = None
+            else:
+                maybe = ctx.me.color
+            #weather alerts
+            try:
+                weatheralert = True
+                a1 = a[0]
+                expiretime = datetime.datetime.fromtimestamp(int(a1.expires)).strftime('%A %B %d, %Y at %I:%M %p')
+                areas = ', '.join(a1.regions)
+                alertresult = "{} in {}. Expires {}. Click [here]({} 'National Weather Service/MET Office') for more information.".format(a1.title, areas, expiretime, a1.uri)
+            except Exception:
+                weatheralert = False
+                pass
+            #uv index
+            if c.uvIndex == 0:
+                whatever = "**{}**. There's no light, or the sun is setting. Darkness is great.".format(c.uvIndex)
+            if c.uvIndex in (1, 2):
+                whatever = "**{}**. There's light, but it's eh.".format(c.uvIndex)
+            if c.uvIndex in (3, 4, 5):
+                whatever = "**{}**. Pretty sunny... Have fun outside.".format(c.uvIndex)
+            if c.uvIndex in (6, 7):
+                whatever = "**{}**. Plenty of sun! Wear sunscreen!.".format(c.uvIndex)
+            if c.uvIndex in (8, 9, 10):
+                whatever = "**{}**. I think it's starting get a bit too much. Get some shade from time to time.".format(c.uvIndex)
+            if c.uvIndex in (11, 12, 13): #fuck going higher
+                whatever = "**{}**. Definitely try to avoid the sunlight at the peak of day, unless you want to burn in hell.".format(c.uvIndex)
+            #icon setup :)
+            if c.icon == 'clear-day':
+                em.set_thumbnail(url='https://dragonfire.me/climacons/Sun.svg')
+            if c.icon == 'clear-night':
+                em.set_thumbnail(url='https://dragonfire.me/climacons/Moon.svg')
+            if c.icon == 'rain':
+                em.set_thumbnail(url='https://dragonfire.me/climacons/Cloud-Rain.svg')
+            if c.icon == 'snow':
+                em.set_thumbnail(url='https://dragonfire.me/climacons/Cloud-Snow.svg')
+            if c.icon == 'sleet':
+                em.set_thumbnail(url='https://dragonfire.me/climacons/Cloud-Drizzle.svg')
+            if c.icon == 'wind':
+                em.set_thumbnail(url='https://dragonfire.me/climacons/Wind.svg')
+            if c.icon == 'fog':
+                em.set_thumbnail(url='https://dragonfire.me/climacons/Cloud-Fog.svg')
+            if c.icon == 'cloudy':
+                em.set_thumbnail(url='https://dragonfire.me/climacons/Cloud.svg')
+            if c.icon == 'partly-cloudy-day':
+                em.set_thumbnail(url='https://dragonfire.me/climacons/Cloud-Sun.svg')
+            if c.icon == 'partly-cloudy-night':
+                em.set_thumbnail(url='https://dragonfire.me/climacons/Cloud-Mood.svg')
+            if c.icon == 'thunderstorm':
+                em.set_thumbnail(url='https://dragonfire.me/climacons/Cloud-Lightning.svg')
+            if c.icon == 'hail':
+                em.set_thumbnail(url='https://dragonfire.me/climacons/Cloud-Hail.svg')
+            #here it begins
+            if farenheit is True:
+                em.add_field(name='Currently', value='{}°F'.format(c.temperature))
+                em.add_field(name='Wind Speed/Gust', value="{} mph/{} mph".format(c.windSpeed, c.windGust))
+                em.add_field(name='Visibility', value="{} miles".format(visib))
+            else:
+                em.add_field(name='Currently', value='{}°C'.format(c.temperature))
+                em.add_field(name='Wind Speed/Gust', value="{} kph/{} kph".format(c.windSpeed, c.windGust))
+                em.add_field(name='Visibility', value="{} kilometers".format(visib))
+            em.color = maybe
+            em.add_field(name='Humidity', value="{:.0%}".format(c.humidity))
+            em.add_field(name='UV Index', value=whatever)
+            if weatheralert is True:
+                em.add_field(name='Weather Alert', value=alertresult)
+            em.set_footer(text="Powered by Dark Sky / Last updated: {}".format(time.strftime("%I:%M:%S %p %Z")), icon_url='https://darksky.net/images/darkskylogo.png')
+            await ctx.send(embed=em)
+        #if anything breaks
         except Exception as e:
-            print("weather alert broke")
-            print(e)
-            weatheralert = False
-        #uv index
-        if c.uvIndex == 0:
-            whatever = "**{}**. There's no light. Darkness is great.".format(c.uvIndex)
-        if c.uvIndex in (1, 2):
-            whatever = "**{}**. There's light, but it's eh.".format(c.uvIndex)
-        if c.uvIndex in (3, 4, 5):
-            whatever = "**{}**. Pretty sunny... Have fun outside.".format(c.uvIndex)
-        if c.uvIndex in (6, 7):
-            whatever = "**{}**. Plenty of sun! Wear sunscreen!.".format(c.uvIndex)
-        if c.uvIndex in (8, 9, 10):
-            whatever = "**{}**. I think it's starting get a bit too much. Get some shade from time to time.".format(c.uvIndex)
-        if c.uvIndex in (11, 12, 13): #fuck going higher
-            whatever = "**{}**. Definitely try to avoid the sunlight at the peak of day, unless you want to burn in hell.".format(c.uvIndex)
-        #here it begins
-        if farenheit is True:
-            em.add_field(name='Currently', value='{}°F {}.'.format(c.temperature, d.summary))
-            em.add_field(name='Wind Speed/Gust', value="{} mph/{} mph".format(c.windSpeed, c.windGust))
-            em.add_field(name='Visibility', value="{} miles".format(visib))
-        else:
-            em.add_field(name='Currently', value='{}°C {}.'.format(c.temperature, d.summary))
-            em.add_field(name='Wind Speed/Gust', value="{} kph/{} kph".format(c.windSpeed, c.windGust))
-            em.add_field(name='Visibility', value="{} kilometers".format(visib))
-        em.color = maybe
-        em.add_field(name='Humidity', value="{:.0%}".format(c.humidity))
-        em.add_field(name='UV Index', value=whatever)
-        if weatheralert is True:
-            em.add_field(name='Weather Alert', value=alertresult)
-        em.set_footer(text="Powered by Dark Sky / Last updated: {}".format(time.strftime("%I:%M:%S %p %Z")), icon_url='https://darksky.net/images/darkskylogo.png')
-    #if anything breaks
-    except Exception as e:
-        await ctx.send("```py\n{}\n```".format(e))
+            await ctx.send("```py\n{}\n```".format(e))
 
-@commands.command()
-async def locate(self, ctx, *, address: str):
-    """Go fucking stalk someone"""
-    try:
-        g = geocoder.google(address)
-        loc = g.json
-        var = json.dumps(loc)
-        k = json.loads(var)
-        if k['status'] == 'OK':
-            yes = k['address']
-        elif k['status'] == 'ZERO_RESULTS':
-            yes = "There's no results found for this location."
-        await ctx.send(yes)
-    except Exception as e:
-        await ctx.send("```py\n{}\n```".format(e))
+    @commands.command()
+    async def locate(self, ctx, *, address: str):
+        """Go fucking stalk someone"""
+        try:
+            g = geocoder.google(address)
+            loc = g.json
+            var = json.dumps(loc)
+            k = json.loads(var)
+            if k['status'] == 'OK':
+                yes = k['address']
+            elif k['status'] == 'ZERO_RESULTS':
+                yes = "There's no results found for this location."
+            await ctx.send(yes)
+        except Exception as e:
+            await ctx.send("```py\n{}\n```".format(e))
 
 def setup(bot):
     bot.add_cog(Weather(bot))
