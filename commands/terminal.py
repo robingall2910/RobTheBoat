590cd9b9bf06cd4c4624157f8078505ae21c1c36
speed <43330808+jsinitx4@users.noreply.github.com>
2019-04-14 18:22:56 -0400

    wait re tard

--- commands/terminal.py
+++ commands/terminal.py
@@ -8,13 +8,13 @@ class Terminal(commands.Cog):
         self.bot = bot
 
     @checks.is_terminal_existent()
-    @commands.command()
+    @commands.command(aliases=['robin', 'pensivr'])
     async def pensive(self, ctx):
         """pensivr"""
         await ctx.send("<@117678528220233731> <@372078453236957185> terminal is dead :crab:")
     
     @checks.is_terminal_existent()
-    @commands.command()
+    @commands.command(aliases=['emoeuropean'])
     async def epic(self, ctx):
         """bruh"""
         await ctx.send("ok... now THIS is epic...")
