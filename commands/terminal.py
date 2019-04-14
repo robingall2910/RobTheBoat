00e237fd0350846e58e6b5ca33f2b6418ea8f1e3
speed <43330808+jsinitx4@users.noreply.github.com>
2019-04-14 16:50:34 -0400

    :crab:

--- commands/terminal.py
+++ commands/terminal.py
@@ -10,8 +10,7 @@ class Terminal(commands.Cog):
     @commands.command
     async def pensive(self, ctx):
         """pensivr"""
-        await ctx.send("<@117678528220233731>")
-
+        await ctx.send("<@117678528220233731> <@372078453236957185> terminal is dead :crab:")
 
 def setup(bot):
     bot.add_cog(Terminal(bot))
