820010ed919b97a1ed02cc4ee66d07c004b45ffe
Robinson Gallego <robinson.leal7@gmail.com>
2019-11-27 23:45:42 -0500

    earrape mode only took 2000 years

--- utils/buildinfo.py
+++ utils/buildinfo.py
@@ -1,7 +1,7 @@
-BUILD_VERSION = "7.8.5"
+BUILD_VERSION = "7.8.7"
 BUILD_AUTHORS = "based robin#0052 and ZeroEpoch1969#0051"
-BUILD_CODENAME = "vsco girls"
-BUILD_DATE = "November 11th, 2019 @ 8:49 PM EST"
+BUILD_CODENAME = "kelly kant kode"
+BUILD_DATE = "November 27th, 2019 @ 11:44 PM EST"
 #how the build version goes:
 #a.b.c
 #a = major rewrites counted
