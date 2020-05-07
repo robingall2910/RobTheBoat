9367e5ef03c339de830962188c542406cd428614
Robinson Gallego <robinson.leal7@gmail.com>
2020-05-06 20:28:52 -0400

    yes it's even further now

--- commands/hypixel.py
+++ commands/hypixel.py
@@ -1,3 +1,5 @@
+import traceback
+
 import discord
 import hypixel
 import asyncio
@@ -17,17 +19,93 @@ class Hypixel(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def hypixeltest(self, ctx, username: str):
-        player = hypixel.Player(username)
-        await ctx.send(f"The player {username} is {player.getRank()['rank']}. That's all I have to say.")
+    @commands.command(aliases=["hinfo"])
+    async def hypixelinfo(self, ctx, username: str):
+        try:
+            player = hypixel.Player(username)
+            embed = discord.Embed(description=None)
+            flogin = player.JSON['firstLogin']
+            llogin = player.JSON['lastLogin']
+            cflogin = datetime.fromtimestamp(flogin/1000.0).strftime('%A, %B %#d, %Y at %#I:%M %p %Z')
+            cllogin = datetime.fromtimestamp(llogin/1000.0).strftime('%A, %B %#d, %Y at %#I:%M %p %Z')
+            if ctx.me.color is not None:
+                embed.color = ctx.me.color
+            try:
+                ltu = hypixel.Player(player.JSON['mostRecentlyTippedUuid']).getName()
+            except KeyError:
+                ltu = "They haven't tipped anyone recently."
+            try:
+                guildname = hypixel.Guild(player.getGuildID()).JSON['name']
+            except Exception: #shut up code convention i dont care
+                guildname = "They aren't in a gang."
+            try:
+                lmv = player.JSON['mcVersionRp']
+            except KeyError:
+                lmv = "They haven't played Minecraft in years, I guess."
+            plevel = player.getLevel()
+            cplevel = '{:,.0f}'.format(plevel)
+            embed.title = f"{player.getName()}'s Hypixel Stats"
+            embed.set_thumbnail(url=f"https://crafatar.com/avatars/{player.UUID}?size=64")
+            embed.add_field(name="Rank", value=f"{player.getRank()['rank']}")
+            embed.add_field(name="Level", value=f"{cplevel}")
+            embed.add_field(name="Guild Name", value=f"{guildname}")
+            embed.add_field(name="First Login", value=f"{cflogin}")
+            embed.add_field(name="Last Login", value=f"{cllogin}")
+            embed.add_field(name="Last Minecraft Version played", value=f"{lmv}")
+            embed.add_field(name="Last Tipped User", value=f"{ltu}")
+            await ctx.send(embed=embed)
+        except hypixel.PlayerNotFoundException:
+            await ctx.send("Player not found! Try another UUID or username.")
+        except Exception:
+            traceback.print_exc()
 
+    @commands.command(aliases=['bedwars', 'binfo', 'bwinfo'])
+    async def hbedwars(self, ctx, username: str):
+        try:
+            player = hypixel.Player(username)
+            embed = discord.Embed(description=f"Level {player.JSON['achievements']['bedwars_level']}")
+            embed.title = f"{player.getName()}'s Bedwars Stats"
+            embed.set_thumbnail(url=f"https://crafatar.com/avatars/{player.UUID}?size=64")
+            if ctx.me.color is not None:
+                embed.color = ctx.me.color
+            try:
+                embed.add_field(name='Beds Broken', value="{:,}".format(player.JSON['achievements']['bedwars_beds']))
+            except KeyError:
+                embed.add_field(name='Beds Broken', value="None. They're innocent, your honor.")
+            embed.add_field(name='Coins', value=f"{player.JSON['stats']['Bedwars']['coins']}")
+            embed.add_field(name='Winstreak', value=f"{player.JSON['stats']['Bedwars']['winstreak']}")
+            embed.add_field(name='Wins', value="{:,}".format(player.JSON['achievements']['bedwars_wins']))
+            embed.add_field(name='Losses', value="{:,}".format(player.JSON['stats']['Bedwars']['losses_bedwars']))
+            embed.add_field(name='Kills', value=f"{player.JSON['stats']['Bedwars']['kills_bedwars']}")
+            embed.add_field(name='Final Kills', value=f"{player.JSON['stats']['Bedwars']['final_kills_bedwars']}")
+            embed.add_field(name='Deaths', value=f"{player.JSON['stats']['Bedwars']['deaths_bedwars']}")
+            embed.add_field(name='Final Deaths', value=f"{player.JSON['stats']['Bedwars']['final_deaths_bedwars']}")
+            embed.add_field(name='Emeralds collected',
+                            value=f"{player.JSON['stats']['Bedwars']['emerald_resources_collected_bedwars']}")
+            embed.add_field(name='Diamonds collected',
+                            value=f"{player.JSON['stats']['Bedwars']['diamond_resources_collected_bedwars']}")
+            embed.add_field(name='Gold collected',
+                            value=f"{player.JSON['stats']['Bedwars']['gold_resources_collected_bedwars']}")
+            embed.add_field(name='Iron collected',
+                            value=f"{player.JSON['stats']['Bedwars']['iron_resources_collected_bedwars']}")
+            wdr = int(player.JSON['achievements']['bedwars_wins'])/int(player.JSON['stats']['Bedwars']['losses_bedwars'])
+            kdr = int(player.JSON['stats']['Bedwars']['kills_bedwars'])/int(player.JSON['stats']['Bedwars']['deaths_bedwars'])
+            fkdr = int(player.JSON['stats']['Bedwars']['final_kills_bedwars']) / int(player.JSON['stats']['Bedwars']['final_deaths_bedwars'])
+            awdr = '{:,.2f}'.format(wdr)
+            akdr = '{:,.2f}'.format(kdr)
+            afkdr = '{:,.2f}'.format(fkdr)
+            embed.add_field(name='Wins/Loss Ratio', value=f"{awdr}")
+            embed.add_field(name='Kill/Death Ratio', value=f"{akdr}")
+            embed.add_field(name='Final Kill/Final Death Ratio', value=f"{afkdr}")
+            await ctx.send(embed=embed)
+        except Exception:
+            traceback.print_exc()
 
     @commands.command()
     @checks.is_dev()
     async def hdebug(self, ctx, *, shit: str):
         try:
-            player = hypixel.Player("usefulnt")
+            player = hypixel.Player("hardnt")
             rebug = eval(shit)
             if asyncio.iscoroutine(rebug):
                 rebug = await rebug
