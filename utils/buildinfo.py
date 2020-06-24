281c01a53c293c3cc97fd2bd86179cd95a6f9482
Robinson Gallego <robinson.leal7@gmail.com>
2020-06-24 01:03:54 -0400

    Care?

--- utils/buildinfo.py
+++ utils/buildinfo.py
@@ -1,7 +1,7 @@
-BUILD_VERSION = "7.8.9"
+BUILD_VERSION = "7.9.0"
 BUILD_AUTHORS = "based robin#0052 and ZeroEpoch1969#0051"
 BUILD_CODENAME = "buy cities: skylines and stomp on ur city"
-BUILD_DATE = "June 21st @ 1:45 PM EDT"
+BUILD_DATE = "June 24th, 2020 @ 1:03 AM EDT"
 #how the build version goes:
 #a.b.c
 #a = major rewrites counted
