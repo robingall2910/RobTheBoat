c0fca95b6e4d938d6c3796a5b191236aa943567f
Robinson Gallego <robinson.leal7@gmail.com>
2020-06-23 23:37:21 -0400

    s

--- commands/hypixel.py
+++ commands/hypixel.py
@@ -92,8 +92,10 @@ class Hypixel(commands.Cog):
             embed.add_field(name='Coins', value=f"{guild.JSON['coins']}")
             embed.add_field(name='Experience', value=f"{guild.JSON['exp']}")
             embed.add_field(name='Joinable?', value=f"{guild.JSON['joinable']}")
-            embed.add_field(name='Created', value=f"{datetime.fromtimestamp(guild.JSON['created'] / 1000.0).strftime('%A, %B %#d, %Y at %#I:%M %p %Z')}")
-            embed.add_field(name="Preferred Games", value=f"\n#1 - {guild.JSON['preferredGames'][0]}\n#2 - {guild.JSON['preferredGames'][1]}\n#3 - {guild.JSON['preferredGames'][2]}\n#4 - {guild.JSON['preferredGames'][3]}", inline=True)
+            embed.add_field(name='Created', value=f"{datetime.fromtimestamp(guild.JSON['created'] / 1000.0).strftime('%A, %B %-d, %Y at %-I:%M %p %Z')}")
+            for s in guild.JSON['preferredGames']:
+                embed.add_field(name='Preferred Games', value=f"{guild.JSON['preferredGames'][s]}")
+            #embed.add_field(name="Preferred Games", value=f"\n#1 - {guild.JSON['preferredGames'][0]}\n#2 - {guild.JSON['preferredGames'][1]}\n#3 - {guild.JSON['preferredGames'][2]}\n#4 - {guild.JSON['preferredGames'][3]}", inline=True)
             await ctx.send(embed=embed)
         except Exception:
             await ctx.send(traceback.format_exc())
