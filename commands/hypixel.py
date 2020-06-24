770c2e30a3f56e9405d3696deac754df9208380c
Robinson Gallego <robinson.leal7@gmail.com>
2020-06-23 22:44:53 -0400

    h

--- commands/hypixel.py
+++ commands/hypixel.py
@@ -76,8 +76,8 @@ class Hypixel(commands.Cog):
         try:
             url = f"https://api.hypixel.net/guild?key={key}&name={gname}"
             r = requests.get(url)
-            gid = r.json()['guild']['_id']
             print(r.json())
+            gid = r.json()['guild']['_id']
             guild = hypixel.Guild(gid)
             playercount = len(guild.JSON['members'])
             embed = discord.Embed(description=f"{guild.JSON['description']}")
