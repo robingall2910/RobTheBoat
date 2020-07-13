eb9ea34602db19e2f2a8ff12d34a88f03c556d74
Robinson Gallego <robinson.leal7@gmail.com>
2020-07-13 18:28:39 -0400

    this should work

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ if config.debug:
 
 bot_triggers = [config.command_prefix, "r.", "hey dragon, ", "hey derg, ", "hey batzz, "]
 
-bot = commands.AutoShardedBot(command_prefix=bot_triggers, description="A bot with several purposes, like music, memes, weather, but based off of the furry fandom.")
+bot = commands.AutoShardedBot(command_prefix=bot_triggers, description="A bot with several purposes, like music, memes, weather, but based off of the furry fandom.", help_command=discord.ext.commands.DefaultHelpCommand(dm_help=True))
 channel_logger = Channel_Logger(bot)
 aiosession = aiohttp.ClientSession(loop=bot.loop)
 lock_status = config.lock_status
